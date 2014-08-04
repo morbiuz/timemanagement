ac4f3f27b129c990d46fcb0cef0de5130eddeb6e
morbiuz <morbiuz@gmail.com>
2014-08-04 13:48:49 +0200

    Added total time spent to project page

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,17 @@ class Project < ActiveRecord::Base
 
 	validates :name, :presence => true
 	validates :user_id, :presence => true
+
+	def total_time
+
+		@total_time = 0
+		@project_shifts = Shift.where("project_id = ?",id)
+		@project_shifts.each do |shift|
+			puts shift.end_date.to_s
+			puts shift.start_date.to_s
+			@total_time += shift.end_date - shift.start_date
+		end
+		return @total_time
+
+	end
 end
