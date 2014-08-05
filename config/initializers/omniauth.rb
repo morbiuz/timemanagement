8b8e40e6e90ac765ccd869d0b4b3a636d5744880
morbiuz <morbiuz@gmail.com>
2014-08-05 19:26:04 +0200

    Working connection to twitter, have to review user validations

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, ENV['Gx0nM6cCRj5Y5VVgXBIEImi2t'], ENV['zkvFWn5mvxr18LuGKBdI33KHdCKZ6tZCKAzVvCOKNlpzgIfvrc']
+  provider :twitter, 'Gx0nM6cCRj5Y5VVgXBIEImi2t', 'zkvFWn5mvxr18LuGKBdI33KHdCKZ6tZCKAzVvCOKNlpzgIfvrc'
+  provider :facebook, '1458084611117134', 'fc47c1c6d3fd14a2dabc28424ee04d61'
 end
