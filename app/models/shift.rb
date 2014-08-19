1a0a4e6a745aab601b0cc41682d93c6bdfbd5f1b
morbiuz <morbiuz@gmail.com>
2014-08-19 18:43:59 +0200

    Shift duration now in hours, modified graph

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -5,8 +5,15 @@ class Shift < ActiveRecord::Base
 	validates :end_date, :presence => true
 	validates_datetime :end_date, :after => :start_date
 
+	before_save :calc_duration
+
 	def to_s
-		@text_duration = "From #{start_date.to_s(:long)} to #{end_date.to_s(:long)}"
+		@text_duration = "From #{start_date.to_s(:long)} to #{end_date.to_s(:long)} (#{duration.to_s} seconds)"
 		return @text_duration
 	end
+
+	def calc_duration
+		self.duration = ((end_date - start_date).round)/3600
+	end
+
 end
