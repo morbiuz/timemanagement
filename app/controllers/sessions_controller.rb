0d3fe474e48d8df4c068fec1b6bfe8682ae53225
morbiuz <morbiuz@gmail.com>
2014-10-06 14:41:35 +0200

    Finished statistics calculation and display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,7 +26,21 @@ class SessionsController < ApplicationController
 			@current_user =  User.find session[:user_id]
 			@total_shifts = Shift.joins(:project).where(projects: { user_id: @current_user.id})
 			@total_hours = @total_shifts.sum(:duration)
+
+			# Calculation of hours per day
 			@hours_day = @total_hours / (( Time.now.to_date - @current_user.created_at.to_date ).to_i + 1)
+
+			# Find the project with most hours
+			project_array = @total_shifts.group(:project_id).sum(:duration).max_by{|k,v| v}
+			@most_hours_project = Project.find_by_id(project_array[0]).name
+			@most_hours = project_array[1]
+
+			# Find the day with most hours
+			day_hash = @total_shifts.group_by_day(:end_date,format: "%D").sum(:duration)
+			@most_productive_day = day_hash.keys[0]
+			@most_productive_day_hours = day_hash.values[0]
+
+
 		else
 			redirect_to :action => 'login'
 		end
